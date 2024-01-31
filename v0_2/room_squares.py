e350272a6eb68c126f10f0acb5aa1b73503b2954
BlueTot <bluetotkc@gmail.com>
2024-01-31 18:27:39 +0000

    version 0.2

---
+++ v0_2/room_squares.py
@@ -0,0 +1,9 @@
+import json
+d = {"start": [(0, 0)],
+     "end": [(0, 0)],
+     "1x1": [(0, 0)],
+     "1x2": [(0, 0), (1, 0)],
+     "2x2": [(0, 0), (1, 0), (0, 1), (1, 1)]
+     }
+with open("room_squares.json", "w") as f:
+    f.write(json.dumps(d, indent=4))
